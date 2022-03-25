729545c2f869f2f019f07f7b0ea80640925fea32
草榴社区 - 简体版 <85705457+t66y-sc@users.noreply.github.com>
2022-03-25 15:02:04 +0800

    Create adxad.js

---
+++ adxad.js
@@ -0,0 +1,5 @@
+document.write(`<div style="display: none; overflow: hidden; text-align: center;">
+  <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621f2f3461d6e260d55ec0c2" async></script><div id="621f2f3461d6e260d55ec0c2" data-width="728" data-height="90" data-output="html"></div>
+  <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621ebd4d61d6e277491eb554" async></script><div id="621ebd4d61d6e277491eb554" data-width="300" data-height="100" data-output="html"></div>
+  <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621f2f7c61d6e260d80557b1" async></script><div id="621f2f7c61d6e260d80557b1" data-width="300" data-height="250" data-output="html"></div>
+</div>`)
