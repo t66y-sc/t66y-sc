0e604735eba1493332b4a07c5456bb35eed0fd11
草榴社区 - 简体版 <85705457+t66y-sc@users.noreply.github.com>
2022-04-03 15:03:58 +0800

    Update adxad.js

--- adxad.js
+++ adxad.js
@@ -1,5 +1,8 @@
-document.write(`<div style="display: none; overflow: hidden; text-align: center;">
+document.write(`<div id="_adxad" style="overflow: hidden; text-align: center;">
   <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621f2f3461d6e260d55ec0c2" async></script><div id="621f2f3461d6e260d55ec0c2" data-width="728" data-height="90" data-output="html"></div>
   <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621ebd4d61d6e277491eb554" async></script><div id="621ebd4d61d6e277491eb554" data-width="300" data-height="100" data-output="html"></div>
   <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621f2f7c61d6e260d80557b1" async></script><div id="621f2f7c61d6e260d80557b1" data-width="300" data-height="250" data-output="html"></div>
-</div>`)
+</div>`);
+setInterval(function(){
+  document.getElementById('_adxad').style.display='none';
+},5000);
