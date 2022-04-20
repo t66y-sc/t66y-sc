868f3022337496b91fe40a5cf78d2cad0ddf64ea
草榴社区 - 简体版 <85705457+t66y-sc@users.noreply.github.com>
2022-04-20 17:30:10 +0800

    Update xads.js

--- xads.js
+++ xads.js
@@ -1,19 +1,24 @@
-;document.write(`
-<style type="text/css">
-	@media screen and (min-width: 728px){
-		#_jads .m1, #_adxad .m1, #_adxad .m2{ display: none }
-	}
-	@media screen and (max-width: 728px){
-		#_jads .pc, #_adxad .pc, #_adxad .m2{ display: none }
-	}
-</style>
+var ismob = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+document.write(`
 <script type="text/javascript" data-cfasync="false" async src="https://t66y-sc.ga/js/jads.js"></script>
 <div id="_jads" style="text-align: center;">
-	<div class="pc"><ins id="973218" data-width="728" data-height="102"></ins></div>
-  <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':973218});</script>
-	<div class="m1"><ins id="975733" data-width="300" data-height="112"></ins></div>
-	<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':975733});</script>
-</div>
+  <div><ins id="973218" data-width="728" data-height="102"></ins></div>
+  <div><ins id="975733" data-width="300" data-height="112"></ins></div>
+</div>`);
+if(!ismob)
+  (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':973218});
+else
+  (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':975733});
+
+document.write(`
+<style type="text/css">
+  @media screen and (min-width: 728px){
+    #_adxad .m1, #_adxad .m2{ display: none }
+  }
+  @media screen and (max-width: 728px){
+    #_adxad .pc, #_adxad .m2{ display: none }
+  }
+</style>
 <div id="_adxad" style="text-align: center;">
   <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621f2f3461d6e260d55ec0c2" async></script><div id="621f2f3461d6e260d55ec0c2" class="pc" data-width="728" data-height="90" data-output="html"></div>
   <script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="621ebd4d61d6e277491eb554" async></script><div id="621ebd4d61d6e277491eb554" class="m1" data-width="300" data-height="100" data-output="html"></div>
