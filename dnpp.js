1c5e1097d6bda1508be05ef2408c4a530b597ded
草榴社区 - 简体版 <85705457+t66y-sc@users.noreply.github.com>
2022-04-30 11:37:36 +0800

    Create dnpp.js

---
+++ dnpp.js
@@ -0,0 +1,51 @@
+// Pop
+if(document.body.clientWidth > 450){
+  var ad_idzone = "4314518",
+    ad_popup_fallback = false,
+    ad_popup_force = false,
+    ad_chrome_enabled = true,
+    ad_new_tab = false,
+    ad_frequency_period = 5,
+    ad_frequency_count = 1,
+    ad_trigger_method = 3,
+    ad_t_venor = false;
+  }else{
+  var ad_idzone = "4342238",
+    ad_popup_fallback = false,
+    ad_popup_force = false,
+    ad_chrome_enabled = true,
+    ad_new_tab = false,
+    ad_frequency_period = 5,
+    ad_frequency_count = 1,
+    ad_trigger_method = 3,
+    ad_t_venor = false;
+}
+document.write('<script type="application/javascript" src="https://a.exdynsrv.com/popunder1000.js"></script>');
+
+// IM
+if(document.body.clientWidth > 450)
+  document.write('<script type="application/javascript" src="https://syndication.exdynsrv.com/splash.php?idzone=4323624"></script>')
+else
+  document.write('<script type="application/javascript" src="https://syndication.realsrv.com/splash.php?idzone=4342236&capping=0"></script>')
+
+// tj
+//!function(){var t=document.createElement("script");function a(){dataLayer.push(arguments)}t.src="https://www.googletagmanager.com/gtag/js?id=UA-195431371-1",t.setAttribute("async",!0),document.head.appendChild(t),window.dataLayer=window.dataLayer||[],a("js",new Date),a("config","UA-195431371-1")}();var _hmt=_hmt||[];!function(){var t=document.createElement("script");t.src="https://hm.baidu.com/hm.js?e3727982f9c7b6a3199de7435a03fda7",t.setAttribute("async",!0),document.head.appendChild(t)}();
+
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4656029,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+  var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+  hs.src = ('//s10.histats.com/js15_as.js');
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e3727982f9c7b6a3199de7435a03fda7";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
